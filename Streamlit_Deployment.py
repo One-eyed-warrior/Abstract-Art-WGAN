79a758ae73a40e9dc1664390abd48612f168000d
Siddhanth Bhattacharyya <ravgersid555@gmail.com>
2024-08-30 23:19:31 +0530

    Update Streamlit_Deployment.py

--- Streamlit_Deployment.py
+++ Streamlit_Deployment.py
@@ -31,7 +31,7 @@ class Generator(nn.Module):
 # Load the trained generator model
 device = torch.device('cpu')  # Change to 'cuda' if you want to load on GPU
 generator = Generator()
-generator.load_state_dict(torch.load('g.pth', map_location=device))
+generator.load_state_dict(torch.load('G.pth', map_location=device))
 generator.eval()
 
 st.set_page_config(page_title="Image Generator", layout="wide")
